477901bfdeae999e4b158e4c694ade00ea473ef0
yaresvc8 <TU_EMAIL_DE_GITHUB>
2025-09-05 22:28:03 -0500

    Añadiendo Main.java con Scanner

--- Main.java
+++ Main.java
@@ -1,46 +1,36 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package com.mycompany.ejercicio1_taller3;
+package com.mycompany.ejercicio2taller3;
+ 
+import java.util.Scanner;
 
 /**
  *
  * @author USUARIO
  */
-class coche {
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("ingrese el valor de a: ");
+        int a = sc.nextInt();
+        
+        System.out.println("ingrese el valor de b: ");
+        int b = sc.nextInt();
+        
+        System.out.println("suma: " + calculadora.sumar(a,b));
+        System.out.println("resta: " + calculadora.restar(a,b));
+        System.out.println("multiplicacion: " + calculadora.multiplicar(a,b));
+        System.out.println("division: " + calculadora.dividir(a,b));
+        
+        sc.close();
+        
+    }
     
-    public String marca;
-    public String modelo;
     
-    public static int contadorCoches = 0;
+        
+    }
+    
 
-    public coche(String marca, String modelo){
-        this.marca = marca;
-        this.modelo = modelo;
-        contadorCoches++;
-    }
-    
-    public void mostrarInfo(){
-        System.out.println("marca: " + marca + ", modelo: " + modelo);
-    }
-    
-    public static void mostrarContador(){
-        System.out.println("Coches creados: " + contadorCoches);
-    }
-}
-
-public class Main { 
-    public static void main(String[] args){
-        
-        coche c1 = new coche("Toyota", "Corolla");
-        coche c2 = new coche ("mazda", "Cx-5");
-        coche c3 = new coche ("Chevrolet", "Spark");
-        
-        c1.mostrarInfo();
-        c2.mostrarInfo();
-        c3.mostrarInfo();
-        
-        coche.mostrarContador();
-   }
-}
